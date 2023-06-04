066057760722954814c9edb6b8f3ca7f46007818
Dhrubojyoti Barman <47059504+dhrubobarman@users.noreply.github.com>
2023-06-04 12:06:54 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ const canvas = document.querySelector<HTMLCanvasElement>(
   "#canvas1"
 ) as HTMLCanvasElement;
 
+const gradientColors = ["#F6FFA6", "#F3BCC8", "#E893CF", "#9376E0"];
+
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -14,6 +16,8 @@ const effect = new Effect({
   ctx,
   mouseRadius: 100,
   numberOfParticles: 300,
+//   gradientColors,
+  friction: 0.1,
 });
 
 function animate() {
